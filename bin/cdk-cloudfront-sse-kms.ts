621f4c0010fc7bba371dc34a3a5bcadfffba2105
Rayjan Wilson <rayjwil@amazon.com>
2022-01-15 17:50:06 -0500

    sorts out naming

--- bin/cdk-cloudfront-sse-kms.ts
+++ bin/cdk-cloudfront-sse-kms.ts
@@ -4,15 +4,15 @@ import { App } from 'aws-cdk-lib';
 import { CdkCloudfrontSseKmsStack } from '../lib/cdk-cloudfront-sse-kms-stack';
 import { KeyStack } from '../lib/kms-keys';
 
-const projectName = 'CloudfrontSseKms';
+const projectName = 'CfSseKms';
 const env = {
   region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
 };
 
 const app = new App();
 
-const keyStack = new KeyStack(app, 'KeyStack', { projectName, env });
-const backend = new CdkCloudfrontSseKmsStack(app, 'CloudfrontSseKmsStack', {
+const keyStack = new KeyStack(app, `${projectName}-KeyStack`, { projectName, env });
+const backend = new CdkCloudfrontSseKmsStack(app, `${projectName}-BackendStack`, {
   projectName,
   env,
 });
